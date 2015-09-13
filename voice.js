7ac28485901cae7e09ff14192ebbc63fb2842c3c
@helloncanella <helloncanella@gmail.com>
2015-09-13 00:11:58 -0300

    first commit

---
+++ voice.js
@@ -0,0 +1,21 @@
+function voice(language, text) {
+  speechSynthesis.cancel(); // destroying past queue
+  var voices = window.speechSynthesis.getVoices();
+  var msg = new SpeechSynthesisUtterance();
+
+  msg.voiceURI = 'native';
+  msg.volume = 1; // 0 to 1
+  msg.rate = 1; // 0.1 to 10
+  msg.pitch = 1; //0 to 2
+
+  for(var i=0; i<voices.length; i++){
+    if(language == voices[i].lang){
+      msg.voice = voices[i];
+    }
+  }
+
+  msg.text = String(text);
+  msg.lang = String(language);
+  speechSynthesis.speak(msg);
+
+}
